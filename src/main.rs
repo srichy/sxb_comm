40b63fd99fecfe0e89196b29fc7ac1ae423007f7
Steven Rich <srichy@mac.com>
2024-01-13 22:09:33 -0500

    clap and top-level arg parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
+use clap::{Parser, ValueEnum};
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about=None)]
+struct Args {
+    #[arg(short, long, value_enum)]
+    action: Action,
+
+    #[arg(help="Filename or address depending on action")]
+    argument: Option<String>,
+}
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum)]
+enum Action {
+    HexDump,
+    ReadBinary,
+    WriteBinary,
+    Execute,
+    DumpRegisters,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Args::parse();
+
+    match cli.action {
+
+        Action::HexDump => {
+        }
+
+        Action::ReadBinary => {
+        }
+
+        Action::WriteBinary => {
+        }
+
+        Action::Execute => {
+        }
+
+        Action::DumpRegisters => {
+        }
+    }
 }
